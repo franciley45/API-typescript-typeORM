6ad6d03d9530d9f0c98ce1227abc8dc6b51b0b70
franciley45 <testepa45@gmail.com>
2023-11-08 12:29:30 -0300

    .

--- src/middleware/ValidateJWT.ts
+++ src/middleware/ValidateJWT.ts
@@ -8,7 +8,7 @@ export const validateJWT = (req: Request, res: Response, next: NextFunction) => 
     console.log(token)
     if (!token) {
       const err = { statusCode: 401, message: 'Token not found' };
-      return res.status(err.statusCode).json(err.message)
+      return res.status(err.statusCode).json({ message: err.message })
     }
     
     /* const tokenSlice = token.slice(7); */
@@ -18,7 +18,7 @@ export const validateJWT = (req: Request, res: Response, next: NextFunction) => 
       next();
     } catch (e) {
       const err = { statusCode: 401, message: 'Expired or invalid token' };
-      return res.status(err.statusCode).json(err.message)
+      return res.status(err.statusCode).json({ message: err.message })
     }
   };
   
