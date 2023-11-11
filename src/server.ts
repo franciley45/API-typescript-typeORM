12517db5a885d50181d69e2a14d3e9972654c497
franciley45 <testepa45@gmail.com>
2023-11-11 17:14:27 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -8,17 +8,37 @@ import routerVideos from "./routes/routerVideos";
 import swaggerUI from "swagger-ui-express";
 import swaggerDocument from "./public/swagger.json"
 import path from "path";
+const {
+    SwaggerUIBundle,
+    SwaggerUIStandalonePreset,
+  } = require("swagger-ui-dist");
+
 
 const app = express();
 
 const port = 3000;
+
 
 app.use(cors());
 app.use(express.json());
 app.use(routerUsers);
 app.use(routerCategories);
 app.use(routerVideos);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+
+
+/* app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument)) */
+
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+
+const options = {
+customCssUrl: CSS_URL,
+};
+
+app.use('/api-docs', swaggerUI.serveFiles(swaggerDocument, options), swaggerUI.setup(swaggerDocument, options));
+
+
+
 app.get('/swagger', (req:Request, res: Response) =>{
     return res.sendFile(path.resolve('src', 'public', 'swagger.json'))
 })
